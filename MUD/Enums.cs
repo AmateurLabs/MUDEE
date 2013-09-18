4b9eb445cec4f6e547ae7067b079d8571d02a48c
Maroy <foemaroy@gmail.com>
2013-09-17 19:12:44 -0500

    Initial Commit

---
+++ MUD/Enums.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MUD
+{
+    public enum Dir
+    {
+        None=-1,
+        North=0,
+        East=1,
+        South=2,
+        West=3,
+        Up=4,
+        Down=5
+    }
+}
---
+++ MUD/Room.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MUD
+{
+    class Room
+    {
+		public string desc;
+        public Room[] neighbors;
+    }
+}
