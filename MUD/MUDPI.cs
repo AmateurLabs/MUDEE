1ea793a4983cd4ec51882c70ac10b42692480dd3
Maroy <foemaroy@gmail.com>
2013-09-20 03:09:32 -0500

    Added Websocket server and MUDPI

---
+++ MUD/MUDPI.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MUDEE {
+	class MUDPI {
+		/* ***Multi-User Domain Protocol Interface***
+		 * MUDPI is very much a client-server protocol, unlike Telnet.
+		 * It operates via escape sequences initiated by BMC (0x91) and terminated by EMC (0x92).
+		 * BMC and EMC are the two C1-block Private Use control characters, as per ECMA-48.
+		 * Commands
+		 */
+
+		public enum CommandCode {
+			BMC = 0x91, //Begin MUDPI Command
+			EMC = 0x92, //End MUDPI Command
+
+		}
+	}
+}
--- MUD/Program.cs
+++ MUD/Program.cs
@@ -9,36 +9,57 @@ namespace MUDEE
     class Program
     {
 		static WebSocketServer server;
+		static List<IWebSocketConnection> clients;
 
         static void Main(string[] args)
         {
+			clients = new List<IWebSocketConnection>();
+			server = new WebSocketServer("ws://www.amateurlabs.com:3000");
+			server.SupportedSubProtocols = new string[] { "mud" };
 			server.Start(socket => {
-				socket.OnBinary = OnBinary;
-				socket.OnClose = OnClose;
-				socket.OnError = OnError;
-				socket.OnMessage = OnMessage;
-				socket.OnOpen = OnOpen;
+				socket.OnBinary = bytes => OnBinary(socket, bytes);
+				socket.OnClose = () => OnClose(socket);
+				socket.OnError = e => OnError(socket, e);
+				socket.OnMessage = msg => OnMessage(socket, msg);
+				socket.OnOpen = () => OnOpen(socket);
 			});
+			bool quit = false;
+			while (!quit) {
+				string input = Console.ReadLine();
+				if (input == "quit") {
+					quit = true;
+				} else {
+					OnServerMsg(input);
+				}
+			}
         }
 
-		static void OnBinary(byte[] bytes) {
-
+		static void OnBinary(IWebSocketConnection socket, byte[] bytes) {
+			
 		}
 
-		static void OnMessage(string msg) {
-
+		static void OnMessage(IWebSocketConnection socket, string msg) {
+			Console.WriteLine("[" + socket.ConnectionInfo.Id + "] " + msg);
 		}
 
-		static void OnClose() {
-
+		static void OnClose(IWebSocketConnection socket) {
+			clients.Remove(socket);
+			Console.WriteLine("Client " + socket.ConnectionInfo.Id + " Disconnected");
 		}
 
-		static void OnError(Exception e) {
-
+		static void OnError(IWebSocketConnection socket, Exception e) {
+			Console.WriteLine("Websocket Error: " + e);
 		}
 
-		static void OnOpen() {
+		static void OnOpen(IWebSocketConnection socket) {
+			clients.Add(socket);
+			Console.WriteLine("Client " + socket.ConnectionInfo.Id + " Connected");
+		}
 
+		static void OnServerMsg(string msg) {
+			foreach (IWebSocketConnection socket in clients) {
+				socket.Send(msg);
+			}
 		}
     }
 }
