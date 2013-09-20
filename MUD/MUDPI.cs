7918692ad348263000ff6481faf39e08df2de0e9
Maroy <foemaroy@gmail.com>
2013-09-20 11:04:51 -0500

    Switched to Exclusive ASCII Support

--- MUD/MUDPI.cs
+++ MUD/MUDPI.cs
@@ -6,26 +6,36 @@ using System.Text;
 namespace MUDEE {
 	class MUDPI {
 		/* ***Multi-User Domain Protocol Interface***
-		 * MUDPI is very much a client-server protocol, unlike Telnet.
-		 * It operates via escape sequences initiated by BMC (0x91) and terminated by EMC (0x92).
+		 * MUDPI is a client-server protocol, unlike Telnet.
+		 * MUDPI supports ASCII, but Unicode support is recommended for message content.
+		 * It operates via escape sequences initiated by MCB and terminated by MCE.
+		 * The client and server must recieve MHS and MHC commands, respectively, before accepting other input.
 		 * BMC and EMC are the two C1-block Private Use control characters, as per ECMA-48.
-		 * Commands 0-31 are reserved in order to prevent conflicts with ASCII Control Characters.
-		 * A server may not use any command besides RCS until it recieves a corresponding CCS command.
+		 * Commands 0-16 and 21-31 are reserved in order to prevent conflicts with ASCII Control Characters.
+		 * A server may not use any command besides CSR and MC_ until it recieves a corresponding CSC command.
 		 * The command Request system applies only to servers; clients can send any commands freely.
-		 * CCS arguments 65-95 are assumed to confirm the corresponding Disable command as well.
-		 * Clients are not obligated to answer RCS commands, but responding with a CCS or DCS is recommended.
-		 * Commands can take a fixed number of non-delimited byte parameters.
+		 * CSC arguments 65-95 are assumed to confirm the corresponding Disable command as well.
+		 * Clients are not obligated to answer CSR commands, but responding with a CSC or CSD is recommended.
+		 * Each command can take any number of byte parameters, but functionality must not change drastically with parameter count.
 		 * Fonts must be monospaced to ensure proper display on non-rectangular buffered clients.
 		 * The formatting codes are designed to roughly match CSS, rather than ANSI.
 		 */
 
 		public enum CommandCode {
-			//0-31: Reserved (ASCII Control Codes)
+			//0-16: Reserved (Non-Printable ASCII Codes)
+
+			//17-20: MUDPI Control Characters
+			MCB = 17, //MUDPI Command Begin
+			MCE = 18, //MUDPI Command End
+			MHS = 19, //MUDPI Handshake Server
+			MHC = 20, //MUDPI Handshake Client
+
+			//21-31: Reserved (Non-Printable ASCII Codes)
 
 			//32, 64, 96: Command Support Codes
-			RCS = 32, //Request Command Support [Command] //The command to request support for. 0=None, 1=All.
-			CCS = 64, //Confirm Command Support [Command]  //The command to confirm support for. 0=None, 1=All.
-			DCS = 96, //Deny Command Support [Command] //The command to deny support for. 0=None, 1=All.
+			CSR = 32, //Command Support Request [Command] //The command to request support for. 0=None, 1=All.
+			CSC = 64, //Command Support Confirm [Command]  //The command to confirm support for. 0=None, 1=All.
+			CSD = 96, //Command Support Deny [Command] //The command to deny support for. 0=None, 1=All.
 
 			//33-47: Get-Set Commands
 			BSG = 33, //Buffer Size Get //Requests that client send its buffer size via BSS.
@@ -43,12 +53,12 @@ namespace MUDEE {
 			CWT = 51, //Clear Window Text
 			//52-57 Reserved
 
-			//58-61: Unused (Custom Client Commands)
+			//58-61: Unused (Custom Commands)
 
 			//62, 63, 95, 127: Extension Commands
-			ECG = 62, //Extension Command Get [Subcommand]
-			ECS = 63, //Extension Command Set [Subcommand, Value]
-			ECE = 95, //Extension Command Enable [Subcommand]
+			ECG = 62, //Extension Command Get [Subcommand, Values...]
+			ECS = 63, //Extension Command Set [Subcommand, Values...]
+			ECE = 95, //Extension Command Enable [Subcommand, Values...]
 			ECD = 127, //Extension Command Disable [Subcommand]
 
 			//65-90: Formatting Enables, 97-122 Formatting Disables
@@ -87,11 +97,8 @@ namespace MUDEE {
 			PSD = 124, //Prompt Scope Disable //Marks the end of the prompt. Fill writeable area between PSE and PSD with spaces.
 			KIE = 93, //Keyboard Input Enable //Allows client to send key info via KSS.
 			KID = 125, //Keyboard Input Disable //Tells client not to send any key info via KSS.
-			//94, 126 Reserved
-
-			//145, 156: MUDPI Control Characters
-			BMC = 145, //Begin MUDPI Command
-			EMC = 146, //End MUDPI Command
+			USE = 94, //Unicode Support Enable
+			USD = 126, //Unicode Support Disable
 		}
 	}
 }
